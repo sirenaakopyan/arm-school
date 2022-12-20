00670902e4796d2276c4d5ef4dcaa00bff0e9d35
Sirena Akopyan <sirenaakopyan@gmail.com>
2022-12-19 22:18:09 -0800

    testing stuff

--- armenian-school/src/App.js
+++ armenian-school/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <ul reversed class="bar">
+            <li><a href="index.html">About</a></li>
+            <li><a href="school.html">School</a></li>
+            <li><a href="camp.html">Camp</a></li>
+            <li><a href="get_involved.html">Get Involved</a></li>
+            <li><a href="vision.html">Vision</a></li>
+          </ul>
+        </div>
       </header>
     </div>
   );
