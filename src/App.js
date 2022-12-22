4a5c4c848ec75f32a8a1c64a7b5ce7680c3f48c0
Sirena Akopyan <sirenaakopyan@gmail.com>
2022-12-22 04:39:43 -0800

    tryna deploy p12312989

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 function App() {
   return (
     <Router>
