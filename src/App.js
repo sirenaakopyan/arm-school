cb437b6f7203a75ca52f5c01ab953eb4e81fcbe4
Sirena Akopyan <sirenaakopyan@gmail.com>
2022-12-22 04:56:59 -0800

    I THINK IT WORKED LETS GOOOOO

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
 
 function App() {
   return (
