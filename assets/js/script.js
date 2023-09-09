84e93f157ac4fffabbd6f7189ef10ef95b997783
Sirena Akopyan <sirenaakopyan@gmail.com>
2023-09-09 02:45:16 -0700

    edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["reading", "writing", "history", "literature", "culture"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
